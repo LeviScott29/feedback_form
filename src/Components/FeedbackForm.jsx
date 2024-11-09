ddd732cbb34ccd6d75c3cd72ea444236b6143605
levi <angel.of.secrets8@gmail.com>
2024-11-09 14:11:17 -0500

    set structure

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 import './FeedbackForm.css'; // Import CSS for styling
-
 const FeedbackForm = () => {
+    const [formData, setFormData] =useState({
+        name: '',
+        email: '',
+        feedback:''
+    });
 
   return (
     <>
@@ -12,6 +16,21 @@ const FeedbackForm = () => {
         <h2>We'd Love to Hear From You!</h2>
         <p>Please share your feedback with us.</p>
         
+        <input 
+        type = "text"
+        name ="name"
+        placeholder= "Your Name"
+        />
+        <input
+        Type ="email"
+        name="email"
+        placeholder = "your email"
+        />
+        <textarea
+        name="feedback"
+        placeholder= "Your Feedback"
+        > </textarea>
+        <button type="submit">Submit Feedback</button>
       </form>
     </>
   );
